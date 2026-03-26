b92b634451a1c1eaef444e045816aa1856a64e52
Musheer0 <musheeran165@gmail.com>
2026-03-26 23:00:13 +0530

    fix generation/id layout

--- src/app/(root)/generations/[id]/page.tsx
+++ src/app/(root)/generations/[id]/page.tsx
@@ -1,13 +1,22 @@
 interface params {
     id :string
 }
+import { Button } from '@/components/ui/button'
+import PageHeader from '@/features/dashboard/page-header'
 import VideoPage from '@/features/videos/components/video-page'
 import React from 'react'
 
 const page = async({params}:{params:Promise<params>}) => {
     const {id} = await params
   return (
-  <VideoPage id={id}/>
+ <div className='flex-1 w-full flex flex-col'>
+  <PageHeader name='Generations'>
+    <Button>
+      Help
+    </Button>
+  </PageHeader>
+   <VideoPage id={id}/>
+ </div>
   )
 }
 
